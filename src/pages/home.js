18678d1cd0d94be53a75d2fcb0aeedf24900dd59
krayzieeddi <63763467+krayzieeddi@users.noreply.github.com>
2023-05-25 12:21:14 -0700

    refactoring start

--- src/pages/home.js
+++ src/pages/home.js
@@ -18,15 +18,32 @@ function Home() {
     navigate("/about")
   }
 
-  return (
-    <div className="Center">
-      <div id="welcome">
+  // welcome greetings section --------------------------
+  const welcomeGreetings = () => {
+    return (
+      <>
+        <div id="welcome">
         <h1>Welcome to CulturalCSBytes!</h1>
         <h2>By All Together Computer Science Education (ATCSED)</h2>
       </div>
-      <button id="login" onClick={login} >Login</button>
-      <button id="createAccount" onClick={createAccount} >Create Account</button>
-      <button id="about" onClick={about} >About</button>
+      </>
+    )
+  } // end of welcome greetings section 
+
+  const homeButtons = () => {
+    return (
+      <>
+        <button id="login" onClick={login} >Login</button>
+        <button id="createAccount" onClick={createAccount} >Create Account</button>
+        <button id="about" onClick={about} >About</button>
+      </>
+    )
+  }
+
+  return (
+    <div className="Center">
+      {welcomeGreetings}
+      {homeButtons}
     </div>
   );
 }
